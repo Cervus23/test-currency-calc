834c4abc5bf4128e86c07d4e265ee08c7f438f98
Cervus_23 <cervus_23@localhost.localdomain>
2020-12-07 21:46:54 +0200

    Updates

---
+++ precache-manifest.30b7ada195d4f66f5836f60ee7a8ba94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494b376aef21463d970ca508e926a19b",
+    "url": "/test-currency-calc/index.html"
+  },
+  {
+    "revision": "522bbf37fe11b6fdd094",
+    "url": "/test-currency-calc/static/css/main.82a7d826.chunk.css"
+  },
+  {
+    "revision": "7441b196363586eeda67",
+    "url": "/test-currency-calc/static/js/2.a5680f22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-currency-calc/static/js/2.a5680f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522bbf37fe11b6fdd094",
+    "url": "/test-currency-calc/static/js/main.2a68ffb9.chunk.js"
+  },
+  {
+    "revision": "6a8630679cafafafb660",
+    "url": "/test-currency-calc/static/js/runtime-main.e6b80a88.js"
+  }
+]);
